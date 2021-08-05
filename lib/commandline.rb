3c8074ab5f9bad359dbcbfc65c87d9b3f72dcecd
Kevin Mugele <kevin.mugele@gmail.com>
2021-08-05 15:38:41 -0400

    Write code and tests to initialize command line

--- lib/commandline.rb
+++ lib/commandline.rb
@@ -1,3 +1,11 @@
 class CommandLine
 
+  attr_reader :input, :output, :message
+
+  def initialize(input, output)
+    @input = input
+    @output = output
+    @message = File.read(input)
+  end
+
 end
