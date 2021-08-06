89c8d9182d737f041e59a5348d349811b2d2b517
Kevin Mugele <kevin.mugele@gmail.com>
2021-08-06 10:19:19 -0400

    Write tests for shifting a character

--- lib/message_shift.rb
+++ lib/message_shift.rb
@@ -1,39 +1,39 @@
 require './lib/shift_creator'
+require './lib/indexable'
 
 class MessageShift
+  include Indexable
 
-  attr_reader :message, :shift_values, :encrypted_message
+  attr_reader :message, :shift_values
 
-  def intitialize(message, shift_values)
+  def initialize(message, shift_values)
     @message = message
     @shift_values = shift_values
   end
 
   def shift_message(shift_values)
-
-  end
-
-  def shift_character(shift_values)
     shifter = 0
-    @message.chars.map do |letter|
-      if shifter = 0
+    encrypted = @message.chars.map do |letter|
+      if shifter == 0
         shifter += 1
-        shift_char(letter, shift_values["A"])
-      elsif shifter = 1
+        shift_char(letter, @shift_values["A"])
+      elsif shifter == 1
         shifter += 1
-        shift_char(letter, shift_values["B"])
-      elsif shifter = 2
+        shift_char(letter, @shift_values["B"])
+      elsif shifter == 2
         shifter += 1
-        shift_char(letter, shift_values["C"])
-      else shifter = 3
+        shift_char(letter, @shift_values["C"])
+      else shifter == 3
         shifter = 0
-        shift_char(letter, shift_values["D"])
+        shift_char(letter, @shift_values["D"])
       end
     end
+    encrypted
   end
 
   def shift_char(letter, shift_value)
-
-
+    index = find_index(letter)
+    new_value = index + shift_value #need to make this only loop through total indexes (27)
+    find_char(new_value)
   end
 end
--- spec/message_shift_spec.rb
+++ spec/message_shift_spec.rb
@@ -2,7 +2,23 @@ require 'date'
 require './lib/enigma'
 require './lib/shift_creator'
 require './lib/message_shift'
+require './lib/indexable'
 
 RSpec.describe MessageShift do
+  it "exists and has attributes" do
+    shift_creator = ShiftCreator.new
+    message_shifter = MessageShift.new("Hello World", shift_creator.find_shift_values("04223", "8152021"))
+
+    expect(message_shifter).to be_a MessageShift
+    expect(message_shifter.message).to eq("Hello World")
+    expect(message_shifter.shift_values).to eq({"A"=>6, "B"=>46, "C"=>30, "D"=>24})
+  end
+
+  it "can shift a character" do
+    shift_creator = ShiftCreator.new
+    message_shifter = MessageShift.new("Hello World", shift_creator.find_shift_values("04223", "8152021"))
+
+    expect(message_shifter.shift_message(shift_values)).to eq()
+  end
 
 end
