6f5772a609d0a99025ff4bb5dca6949c26f92525
Kevin Mugele <kevin.mugele@gmail.com>
2021-08-07 09:54:15 -0400

    Change get_todays_date to be 6 characters in length, add stub for date test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -15,13 +15,18 @@ RSpec.describe Enigma do
   it "#get_todays_date" do
     enigma = Enigma.new
 
-    expect(enigma.get_todays_date).to eq("08062021")
+    allow(enigma).to receive(:get_todays_date).and_return('080521')
+
+    expect(enigma.get_todays_date).to eq("080521")
   end
 
   it "generates random keys" do
     enigma = Enigma.new
 
+    allow(enigma).to receive(:generate_key).and_return('10205')
+
     expect(enigma.generate_key.length).to eq 5
+    expect(enigma.generate_key).to eq("10205")
   end
 
   it "creates encryption hash" do
@@ -40,14 +45,13 @@ RSpec.describe Enigma do
     enigma = Enigma.new
     encrypted = enigma.encrypt("hello world", "02715")
 
-    expect(encrypted).to eq({"date"=>"08062021", "encryption" => "nifaudqdxpy", "key"=>"02715"})
+    expect(encrypted).to eq({"date"=>"080721", "encryption" => "smfazhqdbty", "key"=>"02715"})
   end
 
   it "decrypts a message with a key" do
     enigma = Enigma.new
     encrypted = enigma.encrypt("hello world", "02715")
 
-    expect(enigma.decrypt(encrypted["encryption"], "02715")).to eq({"date"=>"08062021", "decryption" => "hello world", "key"=>"02715"})
+    expect(enigma.decrypt(encrypted["encryption"], "02715")).to eq({"date"=>"080721", "decryption" => "hello world", "key"=>"02715"})
   end
-
 end
