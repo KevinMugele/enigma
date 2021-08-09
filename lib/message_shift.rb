b69358808d4977e225b132d0c8fe8364011caa39
Kevin Mugele <kevin.mugele@gmail.com>
2021-08-09 16:13:51 -0400

    Refactor code

--- lib/message_shift.rb
+++ lib/message_shift.rb
@@ -49,19 +49,23 @@ class MessageShift
     shifter = 0
     shift_values = create_shift_values(number, date)
     decrypted = message.chars.map do |letter|
-      if shifter == 0
-        shifter += 1
-        decrypt_char(letter, shift_values["A"])
-      elsif shifter == 1
-        shifter += 1
-        decrypt_char(letter, shift_values["B"])
-      elsif shifter == 2
-        shifter += 1
-        decrypt_char(letter, shift_values["C"])
-      else shifter == 3
-        shifter = 0
-        decrypt_char(letter, shift_values["D"])
-      end
+      if !create_index.include?(letter)
+        letter
+      else
+        if shifter == 0
+          shifter += 1
+          decrypt_char(letter, shift_values["A"])
+        elsif shifter == 1
+          shifter += 1
+          decrypt_char(letter, shift_values["B"])
+        elsif shifter == 2
+          shifter += 1
+          decrypt_char(letter, shift_values["C"])
+        else shifter == 3
+          shifter = 0
+          decrypt_char(letter, shift_values["D"])
+        end
+      end 
     end
     decrypted.join
   end
