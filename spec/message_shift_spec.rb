aada312c79d10ba2a4cd5c8b21c3d3ba4a01002a
Kevin Mugele <kevin.mugele@gmail.com>
2021-08-09 11:59:57 -0400

    Rename spec file, add module tests to message shift

--- spec/message_shift_spec.rb
+++ spec/message_shift_spec.rb
@@ -24,4 +24,26 @@ RSpec.describe MessageShift do
 
     expect(message_shifter.decrypt_message("keder ohulw", "02715", "040895")).to eq("hello world")
   end
+
+  it "creates index" do
+    message_shifter = MessageShift.new("Hello World")
+
+    expect(message_shifter.create_index).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+  end
+
+  it "#find_index(char)" do
+    message_shifter = MessageShift.new("Hello World")
+
+    expect(message_shifter.find_index("a")).to eq(0)
+    expect(message_shifter.find_index("d")).to eq(3)
+    expect(message_shifter.find_index(" ")).to eq(26)
+  end
+
+  it "#find_char(index)" do
+    message_shifter = MessageShift.new("Hello World")
+
+    expect(message_shifter.find_char(0)).to eq("a")
+    expect(message_shifter.find_char(6)).to eq("g")
+    expect(message_shifter.find_char(26)).to eq(" ")
+  end
 end
